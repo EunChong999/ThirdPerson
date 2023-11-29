8feb30c3c7f6487a6e892baab41d5b957ec02c59
EunChong999 <eunchonggim999@gmail.com>
2023-11-29 23:23:01 +0900

    fix : 점프 애니메이션 끊김 현상 수정중

--- Assets/EunChong/Scripts/Player/Player JumpDown Collision Controller.cs
+++ Assets/EunChong/Scripts/Player/Player JumpDown Collision Controller.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static PlayerMovementController;
 
 public class PlayerJumpDownCollisionController : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         #region JumpDown
-        if (other.gameObject.layer == LayerMask.NameToLayer("whatIsGround") && !PlayerMovementController.Instance.readyToJump)
+        if (other.gameObject.layer == LayerMask.NameToLayer("whatIsGround") && PlayerMovementController.Instance.state == MovementState.ground)  
         {
             PlayerAnimationController.Instance.PlayJumpDownAnim();
         }
